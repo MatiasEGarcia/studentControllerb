21f809db0ea46fda73b9a6a660fa9b0db83381ca
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-01-11 20:14:55 -0300

    first commit

---
+++ src/main/java/com/practice/studentControllerB/StudentControllerBApplication.java
@@ -0,0 +1,13 @@
+package com.practice.studentControllerB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentControllerBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentControllerBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/studentControllerB/StudentControllerBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.studentControllerB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentControllerBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
