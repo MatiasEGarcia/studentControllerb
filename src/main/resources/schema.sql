b6f1d281ffb277f65a3887676cdb7acb37a7bd3c
Matias.E.Garcia <matiasegarcia22@gmail.com>
2023-01-12 13:29:53 -0300

    h2 schema and StudentDaoImplTest

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS students (
+	id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(45) NOT NULL,
+	lastname VARCHAR(45) NOT NULL,
+	email VARCHAR(45) NOT NULL UNIQUE,
+    age TINYINT NOT NULL,
+    admission_date DATE NOT NULL,
+    favorite_language VARCHAR(45),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS teachers (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(45) NOT NULL,
+	lastname VARCHAR(45) NOT NULL,
+	email VARCHAR(45) NOT NULL UNIQUE,
+    age TINYINT NOT NULL,
+    qualification VARCHAR(45) NOT NULL,
+    nacionality VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS courses(
+	id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(45) NOT NULL,
+    teacher INT NOT NULL,
+    shift VARCHAR(45) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(teacher) REFERENCES teachers(id)
+);
+
+CREATE TABLE IF NOT EXISTS qualifications(
+	id INT NOT NULL AUTO_INCREMENT,
+    qualification TINYINT NOT NULL,
+    student INT NOT NULL,
+    course INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY(student) REFERENCES students(id),
+    FOREIGN KEY(course) REFERENCES courses(id)
+);
--- src/test/java/com/practice/studentControllerB/dao/StudentDaoImplTest.java
+++ src/test/java/com/practice/studentControllerB/dao/StudentDaoImplTest.java
@@ -1,14 +1,54 @@
 package com.practice.studentControllerB.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource("/application-test.properties")
+@SpringBootTest
 class StudentDaoImplTest {
 
+	@Autowired
+	private StudentDao dao;
+	
+	@Autowired
+	private JdbcTemplate jdbc;
+	
+	@Value("${sql.script.create.student}")
+	private String sqlAddStudent;
+
+	@Value("${sql.script.truncate.students}")
+	private String sqlTruncateStudents;
+
+	@Value("${sql.script.ref.integrity.false}")
+	private String sqlRefIntegrityFalse;
+
+	@Value("${sql.script.ref.integrity.true}")
+	private String sqlRefIntegrityTrue;
+	
+	@BeforeEach
+	void setUpDatabase() {
+		jdbc.update(sqlAddStudent);
+	}
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void getByEmailReturnNull() {
+		fail();
 	}
 
+	
+	@AfterEach
+	void setUpAfterTransaction() {
+		jdbc.execute(sqlRefIntegrityFalse);
+		jdbc.execute(sqlTruncateStudents);
+		jdbc.execute(sqlRefIntegrityTrue);
+	}
+	
 }
